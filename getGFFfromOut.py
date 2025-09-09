46a9e5f3f61692036b1e2a1a32e70b96233e14e4
Marie <lebherz.marie@gmail.com>
2025-09-09 09:20:36 +0200

    new script

---
+++ getGFFfromOut.py
@@ -0,0 +1,12 @@
+from Bio import SeqIO
+import os
+import subprocess
+from collections import Counter
+import argparse
+import sys
+from Bio import Phylo
+from io import StringIO
+
+"""
+GENERATE AN OUTPUT GFF WITH ALL DESwoMAN NeORFs
+"""
